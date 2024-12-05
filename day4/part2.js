f096d567530d762b4a70dbdb17e6c6023fce9794
Paula Morales <paula.morales@lightspeedhq.com>
2024-12-06 00:32:51 +0100

    optimization day 4 part 2

--- day4/part2.js
+++ day4/part2.js
@@ -10,13 +10,17 @@ const words = ["MAS", "SAM"];
 let counter = 0;
 
 const count = () => {
-  for (let i = 0; i < grid.length - 2; i++) {
-    for (let j = 0; j < grid[0].length - 2; j++) {
-      const char1 = grid[i][j];
-      const char2 = grid[i][j + 2];
-      const char3 = grid[i + 1][j + 1];
-      const char4 = grid[i + 2][j];
-      const char5 = grid[i + 2][j + 2];
+  for (let i = 1; i < grid.length - 1; i++) {
+    for (let j = 0; j < grid[0].length - 1; j++) {
+      if (grid[i][j] !== "A") {
+        continue;
+      }
+
+      const char1 = grid[i - 1][j - 1];
+      const char2 = grid[i - 1][j + 1];
+      const char3 = grid[i][j];
+      const char4 = grid[i + 1][j - 1];
+      const char5 = grid[i + 1][j + 1];
 
       const word1 = `${char1}${char3}${char5}`;
       const word2 = `${char2}${char3}${char4}`;
